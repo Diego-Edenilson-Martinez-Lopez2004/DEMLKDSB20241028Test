0c15a1c8a877bcf1677262f53de4a73745d1add2
KevinSilva <elmersilva462@gmail.com>
2024-10-28 10:23:29 -0600

    metodo buscar para prueba selenium

--- DEMLKDSB20241028Test/TestProducto.cs
+++ DEMLKDSB20241028Test/TestProducto.cs
@@ -52,7 +52,25 @@ namespace DEMLKDSB20241028Test
             System.Threading.Thread.Sleep(1000);
         }
 
+        [TestMethod]
+        public void BuscarProducto()
+        {
+            driver.Navigate().GoToUrl("https://localhost:7159/product-list");
 
+            System.Threading.Thread.Sleep(1000);
+
+            var nombre = driver.FindElement(By.Id("buscar"));
+
+            nombre.SendKeys("Hola");
+
+            System.Threading.Thread.Sleep(1000);
+
+            var buscador = driver.FindElement(By.Name("buscador"));
+
+            buscador.Click();
+
+            System.Threading.Thread.Sleep(1000);
+        }
         public void Dispose()
         {
 
