82634b98ff4060f7ce37a1fefed825ea5c345ccc
Diego-Edenilson-Martinez-Lopez2004 <diego.edenilson.martinez.lopez.n25@gmail.com>
2024-10-28 08:18:10 -0600

    se actualizo el proyecto

--- DEMLKDSB20241028Test/TestBodegaAgape.cs
+++ DEMLKDSB20241028Test/TestBodegaAgape.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DEMLKDSB20241028Test
+﻿namespace DEMLKDSB20241028Test
 {
-    internal class TestBodegaAgape
+    public class TestBodegaAgape
     {
     }
 }
